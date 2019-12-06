c34466a8e53da4eba8406256127159ed83682e70
jamesgoldsmith8 <56980592+jamesgoldsmith8@users.noreply.github.com>
2019-12-05 19:36:06 -0600

    Add abstract folder and give more props to seat and table

--- HighStakes.Domain/Models/Seat.cs
+++ HighStakes.Domain/Models/Seat.cs
@@ -1,8 +1,13 @@
+using System.Collections.Generic;
+using HighStakes.Domain.Abstracts;
+
 namespace HighStakes.Domain.Models
 {
-    public class Seat
+    public class Seat : AGame
     {
         public int SeatId { get; set; }
+        public User Player { get; set; }
+        public List<Card> Hand { get; set; }
         public bool BigBlind { get; set; }
         public bool SmallBlind { get; set; }
     }
--- HighStakes.Domain/Models/Table.cs
+++ HighStakes.Domain/Models/Table.cs
@@ -1,13 +1,13 @@
 using System.Collections.Generic;
+using HighStakes.Domain.Abstracts;
 
 namespace HighStakes.Domain.Models
 {
-    public class Table
+    public class Table : AGame
     {
         public int TableId { get; set; }
         public List<Seat> Seats { get; set; }
         public Deck DeckOfCards { get; set; }
-        public List<Card> ActiveCards { get; set; }
         public List<Pot> Pots { get; set; }
     }
 }
