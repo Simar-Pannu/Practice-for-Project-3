33193e98135118a5234f74f592b85bb3c01c545c
Simar-Pannu <simarpannu@gmail.com>
2019-12-07 19:46:55 -0600

    Fixed repo Account for user

--- HighStakes.Storing/Repositories/UserRepository.cs
+++ HighStakes.Storing/Repositories/UserRepository.cs
@@ -25,7 +25,7 @@ namespace HighStakes.Storing.Repositories
         foreach (var account in db.Accounts)
         {
          if(item.AccountId == account.AccountId){
-           _Users.Add(new DUser(item.UserId,))
+           _Users.Add(new DUser(item.UserId,item.FirstName, item.LastName, item.ChipTotal, new DAccount(account.AccountId, account.UserName, account.Password)));
          }  
         }
           
@@ -33,8 +33,8 @@ namespace HighStakes.Storing.Repositories
 
 
   }
+
+
 */
-
-
   }
 }
