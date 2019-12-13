a41c9f3b034d01daa9340ac535d3193f95d50598
jamesgoldsmith8 <56980592+jamesgoldsmith8@users.noreply.github.com>
2019-12-13 14:16:30 -0600

    new deck tests

--- HighStakes.Testing/DomainTests/DeckTest.cs
+++ HighStakes.Testing/DomainTests/DeckTest.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using HighStakes.Domain.Models;
 using Xunit;
 
@@ -8,7 +9,37 @@ namespace HighStakes.Testing.DomainTests
         [Fact]
         public void Test_Initialize()
         {
+            var deck = new DDeck(0, new List<DCard>());
+
+            deck.Initialize();
+
+            Assert.True(deck.Cards.Count == 52);
+        }
+
+        [Fact]
+        public void Test_Draw()
+        {
+            var deck = new DDeck(0, new List<DCard>());
+            deck.Initialize();
+
+            var card = deck.Draw();
+
+            Assert.True(card.Value > 1);
+        }
+
+        [Fact]
+        public void Test_Shuffle()
+        {
+            var deck = new DDeck(0, new List<DCard>());
+            deck.Initialize();
             
+            var card1 = deck.Cards[0].Name;
+            var card2 = deck.Cards[1].Name;
+            var card3 = deck.Cards[2].Name;
+
+            deck.Shuffle();
+
+            // Assert.False(card1.Equals(deck.Cards[0]))
         }
     }
 }
