505de7f6ee109e850d31730229a76e6eaef8730d
han <han.nguyen@outlook.com>
2019-12-14 15:06:42 -0600

    fixed conflicts

--- HighStakes.Client/Models/Player.cs
+++ HighStakes.Client/Models/Player.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using HighStakes.Domain.Models;
 using HighStakes.Storing.Entities;
 using HighStakes.Storing.Repositories;
-using PizzaBox.Client.Validations;
 
 namespace HighStakes.Client.Models
 {
@@ -13,11 +12,11 @@ namespace HighStakes.Client.Models
     public DUser user { get; set; }
     // public DHand PlayerHand { get; set; }      delete later
     public int userID { get; set; }
-    [NameAttribute(ErrorMessage = "the name must be letters only")]
+
     [StringLength(50)]
     [Required]
+
     public string username { get; set; }
-    [NameAttribute(ErrorMessage = "the name must be letters only")]
     [StringLength(50)]
     [Required]
     public string password { get; set; }
@@ -29,15 +28,18 @@ namespace HighStakes.Client.Models
     public void LoadUser()
     {
       this.user = new DUser();
-      User storingUser = UserRepository.GetUsers().FirstOrDefault(o => o.UserId == this.userID);
+      UserRepository _ur = new UserRepository();
+      // DUser storingUser = _ur.GetUsers().FirstOrDefault(o => o.UserId == this.userID);
+      DUser storingUser = _ur.GetUser(this.userID);
+      this.user = _ur.GetUser(this.userID);
 
-      this.user.UserId = storingUser.UserId;
-      this.user.FirstName = storingUser.FirstName;
-      this.user.LastName = storingUser.LastName;
-      this.user.ChipTotal = storingUser.ChipTotal;
+      // this.user.UserId = storingUser.UserId;
+      // this.user.FirstName = storingUser.FirstName;
+      // this.user.LastName = storingUser.LastName;
+      // this.user.ChipTotal = storingUser.ChipTotal;
 
-      DAccount account = new DAccount(){AccountId=storingUser.Account.AccountId, UserName=storingUser.Account.UserName, Password=storingUser.Account.Password};
-      this.user.Account = account;
+      // DAccount account = new DAccount(){AccountId=storingUser.Account.AccountId, UserName=storingUser.Account.UserName, Password=storingUser.Account.Password};
+      // this.user.Account = account;
     }
     public void CreateUser()
     {
