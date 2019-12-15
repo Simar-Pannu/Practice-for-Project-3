7626319ce73a416bbfd409615eeacb54e5561e44
han <han.nguyen@outlook.com>
2019-12-15 15:45:31 -0600

    removed class1 from storing

---
+++ HighStakes.Storing/Migrations/20191215204330_pipeline-migration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HighStakes.Storing.Migrations
+{
+    public partial class pipelinemigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
