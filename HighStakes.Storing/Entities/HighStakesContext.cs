f3df6b3fd7f43e2acab49247f21208d65594851b
Simar-Pannu <simarpannu@gmail.com>
2019-12-15 15:14:12 -0600

    Back to localhost of sql server

--- HighStakes.Storing/Entities/HighStakesContext.cs
+++ HighStakes.Storing/Entities/HighStakesContext.cs
@@ -13,7 +13,7 @@ namespace HighStakes.Storing.Entities
     protected override void OnConfiguring(DbContextOptionsBuilder dbContext)
     {
    
-      dbContext.UseNpgsql("server=127.0.0.1;database=HighStakes;user id=postgres;password=HighStakes");
+      dbContext.UseNpgsql("server=localhost;database=HighStakes;user id=postgres;password=HighStakes");
       
     }
             
