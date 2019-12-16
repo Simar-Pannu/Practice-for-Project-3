5aee93aa3d4b7be0e9b42aca3918d51c79d599f8
jamesgoldsmith8 <56980592+jamesgoldsmith8@users.noreply.github.com>
2019-12-15 19:43:13 -0600

    d

--- HighStakes.Client/Models/Table.cs
+++ HighStakes.Client/Models/Table.cs
@@ -7,12 +7,11 @@ namespace HighStakes.Client.Models
   [Serializable]
   public class Table
   {
-    public DTable table { get; set; }
-    public int nextTurn { get; set; }
-    public int HighBid { get; set; }
-    public int subround { get; set; }
-    public List<DSeat> seatsOrder { get; set; }
-    public int PotValue { get; set; }
+    public DTable table;
+    public int nextTurn;
+    public int HighBid;
+    public int subround;
+    public List<DSeat> seatsOrder;
 
     public Table()
     {
@@ -31,7 +30,7 @@ namespace HighStakes.Client.Models
         this.table.Flop.Add(this.table.DeckOfCards.Draw());
       }
       this.seatsOrder = this.table.SeatsInTurnOrder;
-      this.PotValue = 0;
+      this.table.CurrentPot.PotValue = 0;
 
     }
     public bool incrementTurn()
